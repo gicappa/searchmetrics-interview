8a30e92bc2ebfecaa0ee337e2ef465433a61dcfc
Gian Carlo Pace <giancarlo.pace@gmail.com>
2021-03-17 09:46:39 +0100

    refactoring the test to factorize common mock in the BeforeEach

--- xchange-api/src/test/java/searchmetrics/RateRestTest.java
+++ xchange-api/src/test/java/searchmetrics/RateRestTest.java
@@ -1,19 +1,32 @@
 package searchmetrics;
 
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class RateRestTest {
+
+    private RateService mockService;
+    private RateResource rateResource;
+
+    @BeforeEach
+    void beforeEach() {
+        mockService = mock(RateService.class);
+        rateResource = new RateResource(mockService);
+
+        when(mockService.getLatestRate()).thenReturn(btcUsdRate14032021_210900());
+    }
 
     @Test
     @DisplayName("It responds with a status code 200")
     void it_returns_200() {
-        var rateResource = new RateResource(mock(RateService.class));
-
         var actual = rateResource.getLatest();
 
         assertThat(actual.getStatus()).isEqualTo(200);
@@ -22,12 +35,14 @@ class RateRestTest {
     @Test
     @DisplayName("It invokes the rate service")
     void it_calls_the_rate_service() {
-        var mockService = mock(RateService.class);
-
-        var rateResource = new RateResource(mockService);
-
-        var actual = rateResource.getLatest();
+        rateResource.getLatest();
 
         verify(mockService).getLatestRate();
     }
+
+    private BtcUsdRate btcUsdRate14032021_210900() {
+        var ts14032021_210900 = LocalDateTime.of(2021, 3, 14, 21, 9, 0);
+        return new BtcUsdRate(1, 60000.15, ts14032021_210900);
+    }
+
 }
